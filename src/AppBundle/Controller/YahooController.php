7bad235f9e2ca94cecf99e020534ddaef5c3aaf3
polidog <polidogs@gmail.com>
2016-12-06 01:51:30 +0900

    user_idの取得まで実装

---
+++ src/AppBundle/Controller/YahooController.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: polidog
+ * Date: 2016/12/06
+ */
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Tavii\OAuth2\Client\Provider\YConnect;
+use Tavii\OAuth2\Client\Provider\YConnectResourceOwner;
+
+/**
+ * @Route("/yahoo")
+ * Class YahooController
+ */
+class YahooController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->get('oauth2.registry')
+            ->getClient('yconnect_client') // key used in config.yml
+            ->redirect();
+    }
+
+    /**
+     * @Route("/callback")
+     *
+     */
+    public function callbackAction()
+    {
+        /** @var YConnect $client */
+        $client = $this->get('oauth2.registry')
+            ->getClient('yconnect_client');
+
+        try {
+            /** @var YConnectResourceOwner $user */
+            $user = $client->fetchUser();
+            var_dump($user->getId());die;
+            // ...
+        } catch (IdentityProviderException $e) {
+            var_dump($e->getMessage());die;
+        }
+    }
+
+}
