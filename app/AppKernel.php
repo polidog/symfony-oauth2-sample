9eecafb2587974e76ca2b312da0628e6cb90a3e3
polidog <polidogs@gmail.com>
2016-12-06 00:20:28 +0900

    AppKernelへの登録

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
+            new KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
